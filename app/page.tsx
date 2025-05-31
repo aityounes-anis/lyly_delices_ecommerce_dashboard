d8d2b7e266ee79c1de16fd014e5abdf58ac9a5e8
aityounes-anis <ma.mohamed.aityounes@gmail.com>
2025-05-31 11:24:49 +0100

    finalizning next.js and shadcnui setup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <section>
+      <h1 className="text-red-500">Test Title</h1>
+      <Button>Shadcn UI Button</Button>
+    </section>
+  );
+}
